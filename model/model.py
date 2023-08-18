dddc52a4c520d938660734938fcececf508e2421
Suting Chen <caoster@icloud.com>
2023-08-18 18:31:36 +0800

    update model.py

---
+++ model/model.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+
+EnergyHistory_data = pd.read_csv("./data/EnergyHistory.csv")
+SunlightHistory_data = pd.read_csv("./data/SunlightHistory.csv")
+
+
+def light_gen_electricity(rad, temp):
+    P_STC = 250
+    G_AC = rad
+    G_STC = 1000
+    T_E = temp
+    T_C = T_E + 30 * G_AC / 1000
+    T_R = 25
+    delta = -0.47 * 0.01
+    P_p_t = P_STC * G_AC * (1 + delta * (T_C - T_R)) / G_STC
+    return P_p_t
+
+
+def calculate_electricity_price(time: datetime):
+    if 8 <= time.hour < 12 or 17 <= time.hour < 21:
+        return 0.628
+    elif 5 <= time.hour < 8 or 12 <= time.hour < 17 or 21 <= time.hour < 22:
+        return 0.529
+    else:
+        return 0.450
+
+
+SunlightHistory_data["Electricity"] = light_gen_electricity(SunlightHistory_data["Radiation"], SunlightHistory_data["Temperature"])
+
+
+class System:
+    def __init__(self, solar: int, battery: int):
+        global EnergyHistory_data, SunlightHistory_data
+        self.solar = solar
+        self.battery = battery
+        self.battery_unit_capacity = 10.5 * 0.9
+        self.cost_solar = 500
+        self.cost_convert = 500
+        self.cost_effi = 0.95
+        self.max_battery = self.battery * self.battery_unit_capacity
+        if self.max_battery > 1500:
+            assert False, "Battery size exceeds limit!"
+        self.cost_battery = 1200 + 2 * self.battery
+        self.current_battery = self.max_battery
+
+        # These two shall be updated together!
+        self.time = datetime(2023, 5, 1, 0)
+        self.step = 0
+
+        # "energy_pv": 0.5500,
+        # "energy_bi": 0.0000,
+        # "energy_bo": 150.0300,
+        # "energy_pg": 0.0000
+
+    def update(self, battery_charge):
+        if not 0 <= battery_charge + self.current_battery <= self.max_battery:
+            assert False, f"Battery amount invalid!, {battery_charge} to {battery_charge + self.current_battery}!"
+        solar = SunlightHistory_data.iloc[self.step]
+        energy = EnergyHistory_data.iloc[self.step]
+        if solar * self.cost_effi < battery_charge:
+            assert False, f"Not enough solar power for charging!"
+
+        self.time += timedelta(hours=1)
+        self.step += 1
+
+
+sy = System(10, 10)
