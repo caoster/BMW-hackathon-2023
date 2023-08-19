5bb364b6069e8b91c60f1e4128b1c76961906255
Suting Chen <caoster@icloud.com>
2023-08-19 10:04:01 +0800

    rename dir

--- submission_script.py
+++ submission_script.py
@@ -1,6 +1,6 @@
 import json
 
-part_1 = json.load(open("./model/part1.json"))
+part_1 = json.load(open("./part1/part1.json"))
 part_2 = json.load(open("./part2/greedy_out.json"))
 
 result = part_1 | part_2
